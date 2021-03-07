81d489cbb21bac7ace37540e8cc90b07d9cdaae9
Shola Anozie <shola@soulprovidr.fm>
2021-03-07 01:03:04 -0500

    Fixed TS errors.

--- src/spotify/hooks/useSpotifyTracks.ts
+++ src/spotify/hooks/useSpotifyTracks.ts
@@ -1,12 +1,16 @@
 import Spotify from 'spotify-web-api-js';
 
-export const useSpotifyTracks = async (resourceUrl: String): void => {
+export const useSpotifyTracks = async (resourceUrl: string): Promise<void> => {
   const spotify = new Spotify();
   spotify.setAccessToken(process.env.SPOTIFY_CLIENT_SECRET);
-  const response = await spotify.search(resourceUrl, [
-    'album',
-    'playlist',
-    'track',
-  ]);
-  console.log(response);
+  try {
+    const response = await spotify.search(resourceUrl, [
+      'album',
+      'playlist',
+      'track',
+    ]);
+    console.log(response);
+  } catch (e) {
+    console.error(e);
+  }
 };
